5f32a4cbddd9ca6a0f57996fb6d4459f99c46799
LucasYanaga <lucasyanaga@live.com>
2022-04-01 08:40:30 -0300

    DynamicStack with responsive print :p

---
+++ src/Stack.java
@@ -0,0 +1,124 @@
+import javax.sound.sampled.Line;
+import java.lang.Math;
+
+public class Stack {
+    public int[] stack;
+    public int index;
+    public int max;
+
+    public Stack(int lenght){
+        this.index = -1;
+        this.stack = new int[lenght];
+    }
+
+    public Stack(int[] stack) {
+        this.index = -1;
+        this.stack = stack;
+    }
+
+    public int[] getStack() {
+        return stack;
+    }
+
+    public void setStack(int[] stack) {
+        this.stack = stack;
+    }
+
+    public void push(int value){
+        System.out.println("Pushing: " + value);
+
+        if (this.isFull()) {
+            increaseStack();
+        }
+
+        this.stack[++this.index] = value;
+    }
+
+    public void pop(){
+        if(!this.isEmpty()){
+            System.out.println("Poping: " + this.stack[this.index]);
+            this.stack[this.index] = 0;
+            this.index--;
+        }
+    }
+
+    public int peek(){
+        if(!this.isEmpty()){
+            return this.stack[this.index];
+        }else{
+            return -1;
+        }
+    }
+
+    public boolean isEmpty(){
+        boolean isEmpty = false;
+
+        if(this.index == -1){
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
+
+    public boolean isFull(){
+        boolean isFull = false;
+
+        if(this.index + 1 == this.stack.length){
+            isFull = true;
+        }
+        return isFull;
+    }
+
+    public void increaseStack(){
+        int[] newStack = new int[this.stack.length + 1];
+
+        for(int i = 0; i < this.stack.length; i++) {
+           newStack[i] = this.stack[i];
+        }
+
+        this.stack = newStack;
+    }
+
+    public void printStack(){
+        this.max();
+        int houses = (int)Math.floor(Math.log10(this.max) + 1);
+
+        for (int i = this.stack.length -1; i >= 0; i--) {
+
+            switch(houses){
+                case 1:
+                    System.out.printf("|%1s%-1d%-1s|\n", "", this.stack[i], "");
+                    break;
+                case 2:
+                    System.out.printf("|%1s%-2d%-1s|\n", "", this.stack[i], "");
+                    break;
+                case 3:
+                    System.out.printf("|%1s%-3d%-1s|\n", "", this.stack[i], "");
+                    break;
+                case 4:
+                    System.out.printf("|%1s%-4d%-1s|\n", "", this.stack[i], "");
+                    break;
+                case 5:
+                    System.out.printf("|%1s%-5d%-1s|\n", "", this.stack[i], "");
+                    break;
+                case 6:
+                    System.out.printf("|%1s%-6d%-1s|\n", "", this.stack[i], "");
+                    break;
+            }
+        }
+
+        for (int i = 0; i < 4 + houses; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public void max(){
+        this.max = 0;
+        for(int i = 0; i < this.stack.length; i++) {
+            if(this.stack[i] > this.max){
+                this.max = this.stack[i];
+            }
+        }
+    }
+
+}
